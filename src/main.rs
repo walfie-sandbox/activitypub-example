0b4313fa9312d1ca5dc7758fcc838b64baf776cd
Walfie <walfington@gmail.com>
2019-09-06 16:55:52 -0400

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 mod activitypub;
 mod webfinger;
 
+use futures_util::TryStreamExt;
+use http_signatures::{CreateKey, ShaSize};
+use hyper::header::{HeaderValue, CONTENT_LENGTH, CONTENT_TYPE};
+use hyper::{Body, Client, Request};
 use ifmt::iformat;
 use openssl::rsa::Rsa;
 use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let domain = "https://example.com";
     let username = "example_user";
 
@@ -17,7 +22,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     let privkey = keypair.private_key_to_der()?;
 
     let pubkey_pem = String::from_utf8(Rsa::public_key_from_der(&pubkey)?.public_key_to_pem()?)?;
-    dbg!(&pubkey_pem);
 
     let person = activitypub::Person {
         context: vec![
@@ -35,8 +39,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         },
     };
 
-    let json = serde_json::to_string(&person)?;
-    dbg!(&json);
+    let person_json = serde_json::to_string(&person)?;
 
     let wf_doc = webfinger::WebFinger {
         subject: &iformat!("acct:{username}@{domain}"),
@@ -48,8 +51,54 @@ fn main() -> Result<(), Box<dyn Error>> {
         }],
     };
 
-    let json = serde_json::to_string(&wf_doc)?;
-    dbg!(&json);
+    let webfinger_json = serde_json::to_string(&wf_doc)?;
 
+    use ring::signature::RSAKeyPair;
+    let json = format!(
+        r#"
+        {{
+            "@context": "https://www.w3.org/ns/activitystreams",
+            "id": "{uri}/statuses/123",
+            "type": "Create",
+            "actor": "{uri}",
+            "object": {{
+                "id": "{uri}/statuses/123",
+                "type": "Note",
+                "published": "2018-06-23T17:17:11Z",
+                "attributedTo": "{uri}",
+                "inReplyTo": "https://example.com/@user/123",
+                "content": "<p>Hello world</p>",
+                "to": "https://www.w3.org/ns/activitystreams#Public"
+            }}
+        }}
+        "#,
+        uri = uri,
+        domain = domain
+    );
+    let mut req = Request::post("http://localhost:3000")
+        .body(json.to_owned().into())
+        .unwrap();
+    req.headers_mut().insert(
+        CONTENT_TYPE,
+        HeaderValue::from_str("application/json").unwrap(),
+    );
+
+    use http_signatures::prelude::WithHttpSignature;
+    req.headers_mut().insert(
+        CONTENT_LENGTH,
+        HeaderValue::from_str(&format!("{}", json.len())).unwrap(),
+    );
+
+    let key_id = &uri;
+    // Add the HTTP Signature
+    let private_key = RSAKeyPair::from_der(untrusted::Input::from(&privkey)).unwrap();
+    req.with_signature_header(key_id.into(), CreateKey::rsa(private_key, ShaSize::SHA256))
+        .unwrap();
+
+    let client = Client::new();
+    let res = client.request(req).await?;
+    println!("POST: {}", res.status());
+
+    let response_body = res.into_body().try_concat().await?;
     Ok(())
 }
