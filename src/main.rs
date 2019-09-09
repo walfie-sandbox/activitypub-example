d41e93fb18b27d5496ca0b1b50852c5aad2ddb30
Walfie <walfington@gmail.com>
2019-09-08 21:56:28 -0400

    Move routes into helper function

--- src/main.rs
+++ src/main.rs
@@ -53,70 +53,73 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
 
     let args = Args::from_args();
     let domain = args.domain;
-
     let repo = Arc::new(InMemoryUserRepository::new(domain.clone()));
 
-    let server = {
-        let user_repo = warp::any().map(move || repo.clone());
-        let domain = warp::any().map(move || domain.clone());
-
-        let well_known = path!(".well-known" / "webfinger")
-            .and(user_repo.clone())
-            .and(warp::query::<WebFingerParams>())
-            .and_then(
-                |repo: Arc<InMemoryUserRepository>, params: WebFingerParams| {
-                    (|| {
-                        if let Some((name, domain)) = parse_acct(&params.resource) {
-                            let user = repo.get_user(&name)?;
-                            let wf_doc = webfinger::WebFinger::from_user(&user, &domain);
-                            let wf_json = serde_json::to_string(&wf_doc)?;
-                            Ok(http::Response::builder()
-                                .header("content-type", "application/json")
-                                .body(wf_json)
-                                .unwrap())
-                        } else {
-                            Ok(http::Response::builder()
-                                .status(http::StatusCode::NOT_FOUND)
-                                .body("".to_string())
-                                .unwrap())
-                        }
-                    })()
-                    .map_err(|e: Box<dyn Error + Send + Sync>| warp::reject::custom(e))
-                },
-            );
-
-        // TODO: Check `accept` header is `application/activity+json`
-        let actor = path!("users" / String).and(domain).and(user_repo).and_then(
-            |username: String, domain: String, repo: Arc<InMemoryUserRepository>| {
-                (|| {
-                    let user = repo.get_user(&username)?;
-                    let person = activitypub::Person::from_user(&user, &domain)?;
-                    let person_json = serde_json::to_string(&person)?;
-
-                    Ok(http::Response::builder()
-                        .header("content-type", "application/activity+json")
-                        .body(person_json)
-                        .unwrap())
-                })()
-                .map_err(|e: Box<dyn Error + Send + Sync>| warp::reject::custom(e))
-            },
-        );
-
-        // TODO: Recover
-        let routes = warp::get2().and(well_known.or(actor)).boxed();
-        warp::serve(routes)
-    }
-    .try_bind(([127, 0, 0, 1], args.port))
-    .compat();
+    let server = warp::serve(routes(domain, repo))
+        .try_bind(([127, 0, 0, 1], args.port))
+        .compat();
 
     log::info!("Starting server on port {}", args.port);
     if server.await == Err(()) {
         panic!("server error");
     }
+
     Ok(())
 }
 
 #[derive(Clone, Deserialize, Debug)]
 struct WebFingerParams {
     resource: String,
+}
+
+fn routes(
+    domain: String,
+    repo: Arc<InMemoryUserRepository>,
+) -> warp::filters::BoxedFilter<(impl warp::Reply,)> {
+    let user_repo = warp::any().map(move || repo.clone());
+    let domain = warp::any().map(move || domain.clone());
+
+    let well_known = path!(".well-known" / "webfinger")
+        .and(user_repo.clone())
+        .and(warp::query::<WebFingerParams>())
+        .and_then(
+            |repo: Arc<InMemoryUserRepository>, params: WebFingerParams| {
+                (|| {
+                    if let Some((name, domain)) = parse_acct(&params.resource) {
+                        let user = repo.get_user(&name)?;
+                        let wf_doc = webfinger::WebFinger::from_user(&user, &domain);
+                        let wf_json = serde_json::to_string(&wf_doc)?;
+                        Ok(http::Response::builder()
+                            .header("content-type", "application/json")
+                            .body(wf_json)
+                            .unwrap())
+                    } else {
+                        Ok(http::Response::builder()
+                            .status(http::StatusCode::NOT_FOUND)
+                            .body("".to_string())
+                            .unwrap())
+                    }
+                })()
+                .map_err(|e: Box<dyn Error + Send + Sync>| warp::reject::custom(e))
+            },
+        );
+
+    // TODO: Check `accept` header is `application/activity+json`
+    let actor = path!("users" / String).and(domain).and(user_repo).and_then(
+        |username: String, domain: String, repo: Arc<InMemoryUserRepository>| {
+            (|| {
+                let user = repo.get_user(&username)?;
+                let person = activitypub::Person::from_user(&user, &domain)?;
+                let person_json = serde_json::to_string(&person)?;
+
+                Ok(http::Response::builder()
+                    .header("content-type", "application/activity+json")
+                    .body(person_json)
+                    .unwrap())
+            })()
+            .map_err(|e: Box<dyn Error + Send + Sync>| warp::reject::custom(e))
+        },
+    );
+
+    warp::get2().and(well_known.or(actor)).boxed()
 }
